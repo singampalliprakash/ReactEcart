a1c53ac48d86bafff747096910cb5b4d084636c5
singampalliprakash <jnanaprakash2002@gmail.com>
2025-05-16 16:36:55 +0530

    errore

--- src/Veg.jsx
+++ src/Veg.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import './npm run buildeg.css';
+import './Veg.css';
 import './App.css';
 import { AddToCart } from './store';
 
